656d6c997b0213862df4a8fbcc12c8115fb4a75a
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-12-09 23:53:46 +0000

    Add logging

--- frontend/app/api/get_api_key/route.ts
+++ frontend/app/api/get_api_key/route.ts
@@ -3,6 +3,7 @@ export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
     const user = request.nextUrl.searchParams.get("user")
+    console.log(`${process.env.ORCHESTRATOR_URL}/getAPIKey?user=${user}`)
     const res = await fetch(`${process.env.ORCHESTRATOR_URL}/getAPIKey?user=${user}`,
         {
             headers: 
