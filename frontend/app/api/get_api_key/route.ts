0f6df0965a4f7c774422a9d05b1329a8c8bd7fea
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-12-10 00:08:04 +0000

    Fix Bug

--- frontend/app/api/get_api_key/route.ts
+++ frontend/app/api/get_api_key/route.ts
@@ -4,7 +4,7 @@ export const runtime = "edge";
 export async function GET(request: NextRequest) {
     const user = request.nextUrl.searchParams.get("user")
     console.log(`${process.env.ORCHESTRATOR_URL}/getAPIKey?user=${user}`)
-    const res = await fetch(`${process.env.ORCHESTRATOR_URL}/getAPIKey?user=${user}`,
+    const res = await fetch(`http://${process.env.ORCHESTRATOR_URL}/getAPIKey?user=${user}`,
         {
             headers: 
                 { 
