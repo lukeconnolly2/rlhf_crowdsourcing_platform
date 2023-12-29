543a543a53362773ea41f794a2427d0882b65e43
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-12-29 00:38:04 +0000

    Fixing the breakage

--- frontend/app/api/route.ts
+++ frontend/app/api/route.ts
@@ -1,7 +1,8 @@
 export const runtime = "edge";
+import { NextResponse } from 'next/server'
 
 export async function GET() {
     const res = await fetch(`http://${process.env.ORCHESTRATOR_URL}/video`, { cache: 'no-store' })
     const videos = await res.json()
-    return Response.json(videos)
+    return NextResponse.json(videos)
   }
