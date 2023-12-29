df22b30767488a1b2143bdb70c1a1c76043d8228
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-12-29 02:31:49 +0000

    fix docker ignore

---
+++ frontend/lib/test-data.ts
@@ -0,0 +1,18 @@
+import { Video } from "@/types/number-card";
+
+export const videos: Video[] = [
+  { id: "v1", views: 0, status: "Unreleased" },
+  { id: "v2", views: 1, status: "Collecting preferences" },
+  { id: "v3", views: 2, status: "Success" },
+  { id: "v4", views: 3, status: "Failed" },
+  { id: "v5", views: 0, status: "Unreleased" },
+  { id: "v6", views: 1, status: "Collecting preferences" },
+  { id: "v7", views: 2, status: "Success" },
+  { id: "v8", views: 3, status: "Failed" },
+  { id: "v9", views: 0, status: "Unreleased" },
+  { id: "v10", views: 1, status: "Collecting preferences" },
+  { id: "v11", views: 2, status: "Unreleased" },
+  { id: "v12", views: 3, status: "Collecting preferences" },
+  { id: "v13", views: 0, status: "Success" },
+  { id: "v14", views: 1, status: "Failed" },
+];
