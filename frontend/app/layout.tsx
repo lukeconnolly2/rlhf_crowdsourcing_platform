33a50691ef23f04c644c0d8b20825cfb2458fb4c
Luke Connolly <luke.connolly2@ucdconnect.ie>
2024-01-16 16:05:16 +0000

    No idea

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css"
-import { Metadata, type Viewport } from "next"
+import { Metadata } from "next"
 import { UserProvider } from "@/providers/user-context"
 import { ClerkProvider } from "@clerk/nextjs"
 import { dark } from "@clerk/themes"
@@ -17,17 +17,15 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-}
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
 }
 
 interface RootLayoutProps {
